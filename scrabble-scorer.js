e7ee1fe75b6532365ef874d746a8e0da5ceffa7f
remi <rachelmilb89@gmail.com>
2024-07-15 02:57:54 -0400

    Final V1

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -34,81 +34,92 @@ function initialPrompt() {
    console.log("Let's play some scrabble!");
    console.log("");
    // oldScrabbleScorer(input.question("Enter a word to score: "));
-  let promptInput = input.question("Enter a word to score: ");
-  return promptInput;
+   let promptInput = input.question("Enter a word to score: ");
+   return promptInput;
 }
 
-let newPointStructure;
+let newPointStructure = transform(oldPointStructure);//transform(this) variable
 
 let simpleScorer = function (word) {
-      word = word.toUpperCase();
-      return word.length;
-   };
+   word = word.toUpperCase();
+   return word.length;
+};
 
 let vowelBonusScorer = function (word) {
-      word = word.toUpperCase();
-      const vowelArray = ["A", "E", "I", "O", "U"];
-      let vowelPoint = 0;
-      let consonantPoint = 0;
-      let totalPoint = 0;
+   word = word.toUpperCase();
+   const vowelArray = ["A", "E", "I", "O", "U"];
+   let vowelPoint = 0;
+   let consonantPoint = 0;
+   let totalPoint = 0;
 
-      for (let letter of word) {
-         if (vowelArray.includes(letter)) {
-            vowelPoint += 3;
-         } else {
-            consonantPoint += 1;
-         }
-         // console.log("vowel", vowelPoint);
-         // console.log("consonant", consonantPoint);
+   for (let letter of word) {//for in would return indices, for of is more readable & returns values
+      if (vowelArray.includes(letter)) {
+         vowelPoint += 3;
+      } else {
+         consonantPoint += 1;
       }
-      totalPoint = vowelPoint + consonantPoint;
-      return totalPoint;
+      // console.log("vowel", vowelPoint);
+      // console.log("consonant", consonantPoint);
    }
+   totalPoint = vowelPoint + consonantPoint;
+   return totalPoint;
+};
 
-let scrabbleScorer;
-
-let userChoice = ""
+let scrabbleScorer = function (word) {
+   let points = 0;
+   for (let letter of word) {
+      letter = letter.toLowerCase();
+      points += newPointStructure[letter];
+   }
+   return points;
+};
 
 const scoringAlgorithms = [
    {
-     name: "Simple Scorer",
-     description: "Each letter is worth 1 point.",
-     scorerFunction: simpleScorer
+      name: "Simple Scorer",
+      description: "Each letter is worth 1 point.",
+      scorerFunction: simpleScorer,
    },
    {
-     name: "Vowel Bonus Scorer",
-     description: "Vowels are worth 3 points, and consonants are worth 1 point.",
-     scorerFunction: vowelBonusScorer
+      name: "Vowel Bonus Scorer",
+      description: "Vowels are worth 3 points, and consonants are worth 1 point.",
+      scorerFunction: vowelBonusScorer,
    },
    {
-     name: "Scrabble Scorer",
-     description: "Uses the standard Scrabble letter values.",
-     scorerFunction: scrabbleScorer
-   }
- ];
+      name: "Scrabble Scorer",
+      description: "Uses the standard Scrabble letter values.",
+      scorerFunction: scrabbleScorer,
+   },
+];
 
-function scorerPrompt(userword) { 
+function scorerPrompt(userword) {
    console.log("Which scoring algorithm would you like to use?");
    console.log("");
    console.log("0 - Simple: One point per character");
    console.log("1 - Vowel Bonus: Vowels are worth 3 points");
    console.log("2 - Scrabble: Uses scrabble point system");
    userChoice = input.question("Enter 0, 1, or 2: ");
-   let selectedScoringAlgorithm = scoringAlgorithms[1];
-   console.log("Score for " + "'" + userword + "'" + ": " + selectedScoringAlgorithm.scorerFunction(userword) );
+   let selectedScoringAlgorithm = scoringAlgorithms[userChoice];
+   console.log(
+      "Score for " +"'" + userword + "'" + ": " + selectedScoringAlgorithm.scorerFunction(userword));
    return selectedScoringAlgorithm;
 }
 
-function transform() { }
+function transform(oldPointStructure) {
+   let transformedOldPtS = {}; // empty obj to hold obj with lowercase keys
+   for (let key in oldPointStructure) {//for in iterates over keys not the values & primarily for objects not arays
+      let valueArray = oldPointStructure[key];
+      for (let character of valueArray) {
+         character = character.toLowerCase();
+         transformedOldPtS[character] = Number(key);
+      }
+   }
+   return transformedOldPtS; // newPointStructure
+}
 
-function runProgram() { 
+function runProgram() {
    let userWord = initialPrompt();
    scorerPrompt(userWord);
-      // Simple scoring
-      // console.log("algorithm name: ", scoringAlgorithms[0].name);
-      // console.log("scorerFunction result: ", scoringAlgorithms[0].scorerFunction("JavaScript"));
-   //console.log(vowelBonusScorer.scoreFunction("helloworld"));
-   //console.log(vowelBonusScorer(input.question("Enter a word to score: ")));
 }
 
 // Don't write any code below this line //
